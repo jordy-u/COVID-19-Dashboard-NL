8f071299cd95ad460479f3964534b900d2ce619f
Jordy Ubink <jordyubink@hotmail.nl>
2020-03-23 19:30:03 +0100

    Implementeer OG-data
    OG-data is de metadata dat weergegeven wordt in Facebook, WhatsApp, Twitter, ect. bij het delen van een link. Dit geeft een beschrijving en een plaatje.

---
+++ assets/og-data.php
@@ -0,0 +1,10 @@
+<?php
+//Informatie die wordt gebruikt in previews. Bijvoorbeeld als de URL gedeeld wordt in WhatsApp
+echo '
+<meta property="og:url" content="https://covid19.jordyu.nl" />
+<meta property="og:site_name" content="COVID19.jordyu.nl" />
+<meta property="og:title" content="COVID-19 Dashboard voor Nederland" />
+<meta property="og:image" content="assets/og-image.jpg" />
+<meta property="og:description" content="Vind hier statistieken over het coronavirus.">
+<meta property="og:locale" content="nl_NL">';
+?>
