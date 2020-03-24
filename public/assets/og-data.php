e92d5cc00c2b29cf1f62d7d7a1a8fad3064d37bb
Jordy Ubink <jordyubink@hotmail.nl>
2020-03-24 22:15:11 +0100

    Fix OG-image 404 error
    The path for the preview-image for social media was incorrect. This is fixed now.

--- public/assets/og-data.php
+++ public/assets/og-data.php
@@ -4,7 +4,7 @@ echo '
 <meta property="og:url" content="https://covid19.jordyu.nl" />
 <meta property="og:site_name" content="COVID19.jordyu.nl" />
 <meta property="og:title" content="COVID-19 Dashboard voor Nederland" />
-<meta property="og:image" content="assets/og-image.jpg" />
+<meta property="og:image" content="/assets/og-image.jpg" />
 <meta property="og:description" content="Vind hier statistieken over het coronavirus.">
 <meta property="og:locale" content="nl_NL">';
 ?>
