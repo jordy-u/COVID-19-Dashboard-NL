bdf64e12b9697c68fff4d18969bb73707d83618b
L Roos <betrayedcover@gmail.com>
2020-03-25 16:11:42 -0700

    gotten plot from data and saved it as SVG (or any other file if desired)

--- Backend/get_csv.py
+++ Backend/get_csv.py
@@ -6,10 +6,11 @@ Created on Wed Mar 25 15:24:33 2020
 """
 
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdatas
 print("reading file")
-path_to_file="../data/rivm_corona_in_nl.csv"
-
+path_to_file="../data/rivm_corona_in_nl_daily.csv"
+save_location="../images/daily_corona_cases"
 try:
     data = pd.read_csv(path_to_file)
 except pd.errors.FileNotFoundError:
@@ -17,9 +18,13 @@ except pd.errors.FileNotFoundError:
         pass
 else:
     print("file found")
-    
 
 print(data.head())
+date = data['Datum']
+aantal = data['Aantal']
 
+plt.plot(date,aantal)
+plt.gcf().autofmt_xdate()
 
+plt.savefig("{}.SVG".format(save_location))
 
