74aceb92e32b1a66d063c90b1ba0890a0097f01a
L Roos <betrayedcover@gmail.com>
2020-03-25 16:21:31 -0700

    made function to save the plot with the mathplotlib library.

--- Backend/get_csv.py
+++ Backend/get_csv.py
@@ -8,11 +8,24 @@ Created on Wed Mar 25 15:24:33 2020
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdatas
+
+def plot_and_safe(datafile,save_location="../images/daily_corona_cases",save_format=".SVG"):
+    date = datafile['Datum']
+    aantal = datafile['Aantal']
+
+    plt.plot(date,aantal)
+    plt.gcf().autofmt_xdate()
+
+    plt.savefig("{}.{}".format(save_location,save_format))
+    print("file created")
+
+
 print("reading file")
-path_to_file="../data/rivm_corona_in_nl_daily.csv"
-save_location="../images/daily_corona_cases"
+path_to_file="../data/rivm_corona_in_nl_daily"
+path_to_save="../images/daily_corona_cases"
+
 try:
-    data = pd.read_csv(path_to_file)
+    data = pd.read_csv("{}.csv".format(path_to_file))
 except pd.errors.FileNotFoundError:
         print("file read error")
         pass
@@ -20,11 +33,6 @@ else:
     print("file found")
 
 print(data.head())
-date = data['Datum']
-aantal = data['Aantal']
 
-plt.plot(date,aantal)
-plt.gcf().autofmt_xdate()
 
-plt.savefig("{}.SVG".format(save_location))
-
+plot_and_safe(data)
