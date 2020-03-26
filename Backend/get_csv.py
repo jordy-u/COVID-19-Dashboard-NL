cfdfec59425c8910dcba29588bf2d919e0ee584d
L Roos <betrayedcover@gmail.com>
2020-03-25 17:31:32 -0700

    made function to load the csv via pandas in a failproof methode

--- Backend/get_csv.py
+++ Backend/get_csv.py
@@ -19,18 +19,26 @@ def plot_and_safe(datafile,save_location="../images/daily_corona_cases",save_for
     plt.savefig("{}.{}".format(save_location,save_format))
     print("file created")
 
+def load_pandas(file_loc='-1'):
+    if file_loc != -1:
+        try:
+            data = pd.read_csv("{}.csv".format(path_to_file))
+        except pd.errors.FileNotFoundError:
+            print("file read error")
+            pass
+        else:
+            print("file found")
+            return(data)
+    else:
+        print("no file path fiven")
 
 print("reading file")
-path_to_file="../data/rivm_corona_in_nl_daily"
+path_to_file="../data/rivm_corona_in_nl"
 path_to_save="../images/daily_corona_cases"
 
-try:
-    data = pd.read_csv("{}.csv".format(path_to_file))
-except pd.errors.FileNotFoundError:
-        print("file read error")
-        pass
-else:
-    print("file found")
+data = load_pandas(path_to_file)
+
+g_code = int(input("Voer gemeentecode in : "))
 
 print(data.head())
 
