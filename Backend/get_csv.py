7ddba436a52a905493fe8d9c1ac8e022ded943a7
L Roos <betrayedcover@gmail.com>
2020-03-25 17:32:06 -0700

    added function to select on base of township and plot the data for that.

--- Backend/get_csv.py
+++ Backend/get_csv.py
@@ -9,14 +9,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdatas
 
-def plot_and_safe(datafile,save_location="../images/daily_corona_cases",save_format=".SVG"):
+def plot_and_save(datafile,save_location="../images/",Save_name='Default',save_format="SVG"):
     date = datafile['Datum']
     aantal = datafile['Aantal']
 
     plt.plot(date,aantal)
     plt.gcf().autofmt_xdate()
 
-    plt.savefig("{}.{}".format(save_location,save_format))
+    plt.savefig("{}{}.{}".format(save_location,Save_name,save_format))
     print("file created")
 
 def load_pandas(file_loc='-1'):
@@ -40,7 +40,12 @@ data = load_pandas(path_to_file)
 
 g_code = int(input("Voer gemeentecode in : "))
 
-print(data.head())
+gemeente_geselecteerde_data = data.loc[data['Gemeentecode'] == g_code]
 
+if gemeente_geselecteerde_data.empty != True:
+    print("Uw gemeentecode is {} , De geselecteerde gemeente is {} Wij halen nu de data op".format(g_code, gemeente_geselecteerde_data.iloc[0][1]))
+    print(gemeente_geselecteerde_data.head())
+    plot_and_save(gemeente_geselecteerde_data,Save_name=g_code)
+else:
+    print("Geen corona in gemeente met code {} gevonden".format(g_code))
 
-plot_and_safe(data)
