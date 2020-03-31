23648ad6874e279b4498bd2618c3f0160721eb22
L Roos <betrayedcover@gmail.com>
2020-03-31 17:31:35 +0200

    read test data from the SQL server

--- Backend/testing_connection.py
+++ Backend/testing_connection.py
@@ -11,7 +11,18 @@ from config import DB_connection_settings
 import mysql.connector
 import mysql.connector
 
-cnx = mysql.connector.connect(user=DB_connection_settings.username, password=DB_connection_settings.password,
-                              host=DB_connection_settings.host,
+
+
+cnx = mysql.connector.connect(user=DB_connection_settings.username(), password=DB_connection_settings.password(),
+                              host=DB_connection_settings.host(),
                               database='test')
+cursor = cnx.cursor()
+
+query = ("SELECT * FROM test")
+
+cursor.execute(query)
+
+for (ID,text) in cursor:
+    print(ID, "   " ,text)
+
 cnx.close()
