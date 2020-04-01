08018ce1de2fd6381393dd87715db79e00db6770
L Roos <betrayedcover@gmail.com>
2020-04-01 10:08:55 +0200

    added first introduction into logging, plus a test connection for internal and external SQL files. using the config and config_test/..py

---
+++ Backend/log_lib.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 09:38:40 2020
+
+@author: User
+"""
+
+
+class event:
+    
+    def file_not_found():
+        print("file read error")
+    
+    def file_found():
+        print("file found")
--- Backend/testing_connection.py
+++ Backend/testing_connection.py
@@ -9,20 +9,28 @@ Created on Tue Mar 31 16:02:06 2020
 from config import DB_connection_settings
 
 import mysql.connector
-import mysql.connector
+from mysql.connector import errorcode
 
-
-
-cnx = mysql.connector.connect(user=DB_connection_settings.username(), password=DB_connection_settings.password(),
+try:
+    cnx = mysql.connector.connect(user=DB_connection_settings.username(), password=DB_connection_settings.password(),
                               host=DB_connection_settings.host(),
                               database='test')
-cursor = cnx.cursor()
-
-query = ("SELECT * FROM test")
-
-cursor.execute(query)
-
-for (ID,text) in cursor:
-    print(ID, "   " ,text)
-
-cnx.close()
+    
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+    cursor = cnx.cursor()
+    
+    query = ("SELECT * FROM test")
+    query2 = ("SELECT COLUMN_NAME FROM information_schema.columns WHERE table_schema='[database]' AND table_name='[table_name]'")
+    cursor.execute(query)
+    
+    for (ID,time,text) in cursor:
+        print("Data gevonden, tegevoegt op {}, met ID {}, Data is alsvolgt {}".format(time,ID,text))
+    
+    cnx.close()
