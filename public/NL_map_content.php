c983a90f77279850ae718a34afce5c0f4ae32550
Jordy Ubink <jordyubink@hotmail.nl>
2020-04-03 22:44:44 +0200

    Fix the bugs that exist online
    The slider had bugs, because events follow a different order than when a server is run locally.

--- public/NL_map_content.php
+++ public/NL_map_content.php
@@ -5,29 +5,6 @@
 //Save the reports here.
 var covid19Reports = null;
 
-//Load map-information
-{
-	var xmlhttp = new XMLHttpRequest();
-	var url = "/assets/NL_kaart/covid19_reports_every_day.json";
-
-	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4) {
-			//Request = OK
-			if (this.status == 200) {
-				covid19Reports = JSON.parse(this.responseText);
-				covid19Reports_Process();
-			}
-			//Request is NOT OK
-			if (this.status != 200) {
-				$("#errorAlert").html('<div class="alert alert-danger" role="alert"><strong>Error: </strong>Het laden van de data is niet gelukt. Probeer het later nog eens.</div>');
-			}
-		}
-	};
-	xmlhttp.open("GET", url, true);
-	xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	xmlhttp.send();
-}
-
 //Fill the map with data of today.
 function covid19Reports_Process() {
 	var amountOfDays = Object.keys(covid19Reports).length;
@@ -36,9 +13,19 @@ function covid19Reports_Process() {
 	var newestMapDate = new Date("2020-02-27");
 	newestMapDate.setDate(newestMapDate.getDate() + amountOfDays - 1);
 	
+	//Update the map
+	updateMap(newestMapDate);
+	
 	//Set max value of slider
 	$("#country-map-slider")[0].setAttribute("max", amountOfDays-1)
 	$("#country-map-slider")[0].setAttribute("value", amountOfDays-1)
+	
+	//Place slider label above the holder (at the end).
+	rangeV.style.setProperty("left", "calc(100% + -10px)")
+	//Convert the selected date to "dd month"
+	const month = newestMapDate.toLocaleString('default', { month: 'long' });
+	var dateString = newestMapDate.getDate().toString() + ' ' + month
+	rangeV.innerHTML = '<span id="current-date">' + dateString + '</span>';
 }
 
 //Update the map with the date in dateString as described in the loaded JSON-file.
@@ -46,12 +33,35 @@ function covid19Reports_Process() {
 function updateMap(date) {
 	var dateString = date.toISOString().substr(0,10)
 	var dataSelectedDay = covid19Reports[dateString];
-	console.log("test1");
 	$( ".st0", $("#gemeentes")[0] ).each(function() {
 		color = dataSelectedDay[this.id]/571.0*255;
 		this.setAttribute("style", "fill: rgb(255," + (255-color) + "," + (255-color) + ")");
 	});
 }
+$(document).ready(function(){
+	//Load map-information
+	{
+		var xmlhttp = new XMLHttpRequest();
+		var url = "/assets/NL_kaart/covid19_reports_every_day.json";
+
+		xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4) {
+				//Request = OK
+				if (this.status == 200) {
+					covid19Reports = JSON.parse(this.responseText);
+					covid19Reports_Process(); 
+				}
+				//Request is NOT OK
+				if (this.status != 200) {
+					$("#errorAlert").html('<div class="alert alert-danger" role="alert"><strong>Error: </strong>Het laden van de data is niet gelukt. Probeer het later nog eens.</div>');
+				}
+			}
+		};
+		xmlhttp.open("GET", url, true);
+		xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+		xmlhttp.send();
+	}  
+});
 </script>
 
 <div id="errorAlert"></div>
@@ -75,10 +85,15 @@ const
 		var mapDate = new Date("2020-02-27");
 		var daysLater = parseInt(range.value);
 		mapDate.setDate(mapDate.getDate() + daysLater);
-		updateMap(mapDate)
-		const month = mapDate.toLocaleString('default', { month: 'long' });
 		
+		//Don't update the map if the reports are not downloaded yet (when this webpage is loaded).
+		if (covid19Reports)
+			updateMap(mapDate)
+		
+		//Convert the selected date to "dd month"
+		const month = mapDate.toLocaleString('default', { month: 'long' });
 		var dateString = mapDate.getDate().toString() + ' ' + month
+		
 		const
 			newValue = Number( (range.value - range.min) * 100 / (range.max - range.min) ),
 			newPosition = 10 - (newValue * 0.2);
