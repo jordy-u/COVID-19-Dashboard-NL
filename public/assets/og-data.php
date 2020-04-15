2faa7996efb98e4bd700809c2b0b79bbf80fb2b2
Jordy Ubink <jordyubink@hotmail.nl>
2020-04-15 23:38:18 +0200

    update OG:data and readme.md
    Update the metadata that the website shows in WhatsApp, Twitter, ect.

--- public/assets/og-data.php
+++ public/assets/og-data.php
@@ -1,10 +1,10 @@
 <?php
 //Informatie die wordt gebruikt in previews. Bijvoorbeeld als de URL gedeeld wordt in WhatsApp
 echo '
-<meta property="og:url" content="https://covid19.jordyu.nl" />
-<meta property="og:site_name" content="COVID19.jordyu.nl" />
+<meta property="og:url" content="https://coronaperdag.nl/" />
+<meta property="og:site_name" content="coronaperdag.nl" />
 <meta property="og:title" content="COVID-19 Dashboard voor Nederland" />
 <meta property="og:image" content="/assets/og-image.jpg" />
-<meta property="og:description" content="Vind hier statistieken over het coronavirus.">
+<meta property="og:description" content="Kijk terug in de tijd en zie hoe het coronavirus zich heeft versprijd in Nederland en in jouw regio.">
 <meta property="og:locale" content="nl_NL">';
 ?>
