751b34f07c00c1f303cdcd1bc7d1cf8b553ad37c
Jordy Ubink <jordyubink@hotmail.nl>
2020-04-22 11:35:12 +0200

    Split slider script from php file.
    This makes things more organised.

--- public/assets/NL_kaart/NL_map_content.php
+++ public/assets/NL_kaart/NL_map_content.php
@@ -35,31 +35,4 @@
 <br>
 
 <!-- Script to use the slider -->
-<script>
-//https://codepen.io/onyx1812/pen/GRJxmva
-const
-	range = document.getElementById('country-map-slider'),
-	rangeV = document.getElementById('rangeV'),
-	setValue = ()=>{
-		if (selectedDataset == null) return;
-		//var mapDate = new Date(selectedDataset.startDate);
-		var daysLater = parseInt(range.value);
-		
-		var mapDate = addDaysToDate(selectedDataset.startDate, daysLater);
-		
-		//Don't update the map if the reports are not downloaded yet (when this webpage is loaded).
-		if (selectedDataset.data)
-			updateMap(mapDate)
-		
-		//Convert the selected date to "dd month"
-		var dateString = getDayAndMonth(mapDate)
-		
-		const
-			newValue = Number( (range.value - range.min) * 100 / (range.max - range.min) ),
-			newPosition = 10 - (newValue * 0.2);
-		rangeV.innerHTML = `<span id="current-date">${dateString}</span>`;
-		rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
-	};
-document.addEventListener("DOMContentLoaded", setValue);
-range.addEventListener('input', setValue);
-</script>
+<script type="text/javascript" src="/assets/NL_kaart/NL_map_slider.js"></script>
